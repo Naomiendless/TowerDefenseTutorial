94842965efcac4e6472160dfc3089578cb3e0f14
endlessclouds <naomi@endlesscloud.xyz>
2022-06-06 20:07:55 +0800

    E10 selection

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private bool gameEnded = false;
+    public static bool GameIsOver = false;
+
+    public GameObject gamoeverUI;
+
+    void Start()
+    {
+        GameIsOver = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (gameEnded)
+        if (GameIsOver)
             return;
-        
+
         if (PlayerStats.Lives <= 0)
         {
             EndGame();
@@ -19,7 +28,7 @@ public class GameManager : MonoBehaviour
 
     private void EndGame()
     {
-        gameEnded = true;
-        Debug.Log("Game Over!");
+        GameIsOver = true;
+        gamoeverUI.SetActive(true);
     }
 }
