9b2bbf5584801022da240d647a0ce8b5cd9d90c3
endlessclouds <naomi@endlesscloud.xyz>
2022-06-06 12:06:38 +0800

    Tower Defense Game E16 Slowing

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float startSpeed = 10f;
+    [HideInInspector]
+    public float speed;
+
+    public float health = 100;
+
+    public int worth = 50;
+
+    public GameObject deathEffect;
+    
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Slow(float pct)
+    {
+        speed = startSpeed * (1f - pct);
+    }   
+    void Die()
+    {
+        GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        
+        PlayerStats.Money += worth;
+        Destroy(gameObject);
+    }
+
+
+}
