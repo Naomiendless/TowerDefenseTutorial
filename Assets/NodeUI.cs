6d0b180f3799a11244632473d8fb1802c5fdbea9
endlessclouds <naomi@endlesscloud.xyz>
2022-06-07 17:48:10 +0800

    E20 Sell

--- Assets/NodeUI.cs
+++ Assets/NodeUI.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeUI : MonoBehaviour
 {
     public GameObject ui;
+    public Text upgradeCost;
+    public Button upgradeButton;
+    public Text sellAmount;
+    public Button sellButton;
     
     private Node target;
+    
     public void SetTarget(Node _target)
     {
         target = _target;
 
         transform.position = target.GetBuildPosition();
+        
+        if (!target.isUpgraded)
+        {
+            upgradeCost.text = "$" + target.turretBlueprint.upgradeCost.ToString();
+            upgradeButton.interactable = true;
+        }
+        else
+        {
+            upgradeCost.text = "DONE";
+            upgradeButton.interactable = false;
+        }
+
+        sellAmount.text = "$" + target.turretBlueprint.GetSellAmount().ToString();
         
         ui.SetActive(true);
     }
@@ -17,6 +36,18 @@ public class NodeUI : MonoBehaviour
     public void Hide()
     {
         ui.SetActive(false);
+    }
+
+    public void Upgrade()
+    {
+        target.UpgradeTurret();
+        BuildManager.instance.DeselectNode();
+    }
+
+    public void Sell()
+    {
+        target.SellTurret();
+        BuildManager.instance.DeselectNode();
     }
 }
     
